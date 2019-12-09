3ce36980fa1184d829fd186eb0640df48118da77
fatmake <fatmakelantan@outlook.com>
2019-12-09 16:19:57 +0300

    HW8

--- mbta.rb
+++ mbta.rb
@@ -1,8 +1,4 @@
-
-
-    def stops_between_stations(start_line,start_station,end_line,end_station)
-
-      $subway_lines = {
+ $subway_lines = {
         red: ["South Station",
             "Kendall",
             "Central",
@@ -28,7 +24,8 @@
             "Forest Hills"]
     }
 
-
+ def stops_between_stations(start_line,start_station,end_line,end_station)
+      
     $stops =0
     $total_stops=0
 
@@ -36,18 +33,23 @@
             indexof_end_station = $subway_lines[end_line.downcase.to_sym].index(end_station)
 
             if start_line == end_line
+
                 stops = (indexof_start_station-indexof_end_station).abs
                 puts stops.to_s+" stops"
+                
             end
 
             if start_line != end_line
+
                 indexof_parkStreet_start = $subway_lines[start_line.downcase.to_sym].index("Park Street")
                 indexof_parkStreet_end = $subway_lines[end_line.downcase.to_sym].index("Park Street")
-                park_street_start_line = %(#{indexof_start_station - indexof_parkStreet_start}).abs
-                park_street_end_line = %(#{indexof_end_station - indexof_parkStreet_end}).abs
+                park_street_start_line = (indexof_start_station - indexof_parkStreet_start).abs
+                park_street_end_line = (indexof_end_station - indexof_parkStreet_end).abs
                 total_stops = park_street_start_line + park_street_end_line
-                puts total_stops+ " stops"
+                puts total_stops.to_s+" stops"
+
             end
+
     end
 
 stops_between_stations('Red', 'Alewife', 'Red', 'Park Street') #3 stops
@@ -57,24 +59,24 @@ stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') # 7 stops
 
  ######bonus########
 
-p "Enter your statrt line"
+#p "Enter your statrt line"
 
-user_input1 = gets
+#user_input1 = gets
 
-p "Enter your statrt station"
+#p "Enter your statrt station"
 
-user_input2 = gets
+#user_input2 = gets
 
-p "Enter your end line"
+#p "Enter your end line"
 
-user_input3 = gets
+#user_input3 = gets
 
-p "Enter your end station"
+#p "Enter your end station"
 
-user_input4 = gets
+#user_input4 = gets
 
 
-stops_between_stations(user_input1, user_input2, user_input3, user_input4)
+#stops_between_stations(user_input1, user_input2, user_input3, user_input4)
 
 
 
